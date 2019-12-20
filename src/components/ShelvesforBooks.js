5b3269417130d4a8eac847409ca7a8f296bf91e6
Kaustav Karmakar <kaustavkarmakar@Kaustavs-MacBook-Air.local>
2019-12-20 20:37:19 +0530

    kk

--- src/components/ShelvesforBooks.js
+++ src/components/ShelvesforBooks.js
@@ -2,47 +2,67 @@ import React from "react";
 import BookShelf from "./BookShelf";
 
 class ShelvesForBooks extends React.Component {
+  state = {
+    books: [],
+    shelf: ""
+  };
   render() {
-    // const shelves = {
-    //     currentlyReading: ['Currently Reading', 'currentlyReading'],
-    //     wantToRead: ['Want to Read', 'wantToRead'],
-    //     read: ['Read', 'read']
-    // }
-    const allBooksForSheveles = this.props.allBooksForSheveles;
-    const currentlyReading = allBooksForSheveles.filter(
-      book => book.shelf === "currentlyReading"
-    );
-    const wantToRead = allBooksForSheveles.filter(
-      book => book.shelf === "wantToRead"
-    );
-    const read = allBooksForSheveles.filter(book => book.shelf === "read");
-    console.log("fff", allBooksForSheveles);
+    const shelves = {
+      currentlyReading: ["Currently Reading", "currentlyReading"],
+      wantToRead: ["Want to Read", "wantToRead"],
+      read: ["Read", "read"]
+    };
+    console.log(shelves);
+    //  const allBooksForSheveles = this.props.allBooksForShevele;
+    // const currentlyReading = allBooksForSheveles.filter(
+    //   book => book.shelf === "currentlyReading"
+    // );
+    // const wantToRead = allBooksForSheveles.filter(
+    //   book => book.shelf === "wantToRead"
+    // );
+    // const read = allBooksForSheveles.filter(book => book.shelf === "read");
+    //  console.log("fff", allBooksForSheveles);
 
     return (
       <div className="list-books-content">
-        <div>
-          {/*ShelvesForBooks-currentely read*/}
-          <BookShelf
-            books={currentlyReading}
-            title={"Currently Reading"}
-            changeShelf={this.props.changeShelf}
-          />
-          {/* <BookShelf changeShelf={this.renderObj()}/> */}
-          {/*ShelvesForBooks-want to read*/}
-          <BookShelf
-            books={wantToRead}
-            title={"Want To Read"}
-            changeShelf={this.props.changeShelf}
-          />
-          {/*ShelvesForBooks-read*/}
-          <BookShelf
-            books={read}
-            title={"Read"}
-            changeShelf={this.props.changeShelf}
-          />
+        <div className="list-books-content">
+          {Object.keys(shelves).map(shelf => (
+            <BookShelf
+              key={shelf}
+              shelf={shelves[shelf][1]}
+              title={shelves[shelf][0]}
+              books={this.state.books}
+              onShelfChange={() => {
+                this.changeShelf(shelf);
+              }}
+            />
+          ))}
         </div>
       </div>
     );
   }
 }
 export default ShelvesForBooks;
+
+
+  /* <div>
+       
+          <BookShelf
+            books={currentlyReading}
+            title={"Currently Reading"}
+            changeShelf={this.props.changeShelf}
+          />
+          
+          <BookShelf
+            books={wantToRead}
+            title={"Want To Read"}
+            changeShelf={this.props.changeShelf}
+          />
+        
+          <BookShelf
+            books={read}
+            title={"Read"}
+            changeShelf={this.props.changeShelf}
+          />
+        </div> */
+
